452caaf1f910b9f95ebc686f106d8497f4f57c7d
Joshua Hoffman <joshuahoffman2023@u.northwestern.edu>
2022-03-30 17:56:40 -0500

    read from a database

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import CourseList from "./components/CourseList";
+import { useData } from "./utilities/firebase.js";
 
 const Banner = ({ title }) => <h1 className="mt-2">{title}</h1>;
 
@@ -31,20 +32,10 @@ const timeParts = (meets) => {
 };
 
 const App = () => {
-	const [schedule, setSchedule] = useState();
-	const url = "https://courses.cs.northwestern.edu/394/data/cs-courses.php";
+	const [schedule, loading, error] = useData("/", addScheduleTimes);
 
-	useEffect(() => {
-		const fetchSchedule = async () => {
-			const response = await fetch(url);
-			if (!response.ok) throw response;
-			const json = await response.json();
-			setSchedule(addScheduleTimes(json));
-		};
-		fetchSchedule();
-	}, []);
-
-	if (!schedule) return <h1>Loading schedule...</h1>;
+	if (error) return <h1>{error}</h1>;
+	if (loading) return <h1>Loading the schedule...</h1>;
 
 	return (
 		<div className="container m-4">
---
+++ src/utilities/firebase.js
@@ -0,0 +1,49 @@
+import { initializeApp } from "firebase/app";
+import { useState, useEffect } from "react";
+import { getDatabase, onValue, ref, set } from "firebase/database";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyAEGXeUFCowk8341C0DZ0ZqU_utvbTRcDA",
+	authDomain: "cs394-scheduler-e3f97.firebaseapp.com",
+	databaseURL: "https://cs394-scheduler-e3f97-default-rtdb.firebaseio.com",
+	projectId: "cs394-scheduler-e3f97",
+	storageBucket: "cs394-scheduler-e3f97.appspot.com",
+	messagingSenderId: "442130159499",
+	appId: "1:442130159499:web:9474dc24712b54cc7fc18e",
+};
+
+const firebase = initializeApp(firebaseConfig);
+const database = getDatabase(firebase);
+
+export const useData = (path, transform) => {
+	const [data, setData] = useState();
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState();
+
+	useEffect(() => {
+		const dbRef = ref(database, path);
+		const devMode = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
+		if (devMode) {
+			console.log(`loading ${path}`);
+		}
+		return onValue(
+			dbRef,
+			(snapshot) => {
+				const val = snapshot.val();
+				if (devMode) {
+					console.log(val);
+				}
+				setData(transform ? transform(val) : val);
+				setLoading(false);
+				setError(null);
+			},
+			(error) => {
+				setData(null);
+				setLoading(false);
+				setError(error);
+			}
+		);
+	}, [path, transform]);
+
+	return [data, loading, error];
+};
